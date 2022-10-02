ab7a5a04be37d2c166bfc5522d30550776f1a359
Ibrahim <ibrahimghali211@gmail.com>
2022-10-02 18:12:30 +0300

    write better code

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,25 +5,38 @@ const getTasks = (req, res) => {
 };
 
 const getTask = (req, res) => {
-  const task = tasks.find(task => task.id === Number(req.params.id));
-  res.status(200).json(task);
+  const task = tasks.find(task => task.id === req.params.id);
+  if (!task) {
+    res.status(404).json({ msg: `No task with the id: ${req.params.id}` });
+  } else {
+    res.status(200).json(task);
+  }
 };
 
 const createTask = (req, res) => {
-  const task = { id: Date.now(), title: req.body.title };
+  const task = { id: String(Date.now()), title: req.body.title };
   tasks.unshift(task);
   res.status(201).json({ msg: 'New task added' });
 };
 
 const updateTask = (req, res) => {
-  const task = tasks.find(task => task.id === Number(req.params.id));
-  task.title = req.body.title;
-  res.status(200).json({ msg: 'Task updated' });
+  const task = tasks.find(task => task.id === req.params.id);
+  if (!task) {
+    res.status(404).json({ msg: `No task with the id: ${req.params.id}` });
+  } else {
+    task.title = req.body.title;
+    res.status(200).json({ msg: 'Task updated' });
+  }
 };
 
 const deleteTask = (req, res) => {
-  tasks = tasks.filter(task => task.id !== Number(req.params.id));
-  res.status(200).json({ msg: 'Task deleted' });
+  const task = tasks.find(task => task.id === req.params.id);
+  if (!task) {
+    res.status(404).json({ msg: `No task with the id: ${req.params.id}` });
+  } else {
+    tasks = tasks.filter(task => task.id !== req.params.id);
+    res.status(200).json({ msg: 'Task deleted' });
+  }
 };
 
 module.exports = {
